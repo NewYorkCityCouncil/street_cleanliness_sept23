c6a000019e462eb6fe01b47d2f0982d35f530e2e
Brook Frye <bfrye@council.nyc.gov>
2023-09-01 11:00:34 -0400

    add packgs

--- code/00_load_dependencies.R
+++ code/00_load_dependencies.R
@@ -5,7 +5,7 @@
 #' 
 #' IF YOU DO NOT WANT TO INSTALL ANY OF THESE PACKAGES, DO NOT RUN THIS CODE.
 
-list.of.packages <- c("tidyverse", "janitor", "vroom")
+list.of.packages <- c("tidyverse", "janitor", "vroom", "data.table", "leaflet", "sf", "ggplot", "stringr")
 
 # checks if packages has been previously installed
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -24,3 +24,20 @@ lapply(c(list.of.packages,"councilverse"), require, character.only = TRUE)
 rm(list.of.packages,new.packages,councilverse.check)
 
 ## Functions -----------------------------------------------
+
+# unzip function
+unzip_sf <- function(zip_url) {
+  temp <- tempfile()
+  temp2 <- tempfile()
+  #download the zip folder from the internet save to 'temp' 
+  download.file(zip_url, temp)
+  #unzip the contents in 'temp' and save unzipped content in 'temp2'
+  unzip(zipfile = temp, exdir = temp2)
+  #if returns "character(0), then .shp may be nested within the folder
+  your_SHP_file <- ifelse(!identical(list.files(temp2, pattern = ".shp$",full.names=TRUE), character(0)), 
+                          list.files(temp2, pattern = ".shp$",full.names=TRUE), 
+                          list.files(list.files(temp2, full.names=TRUE), pattern = ".shp$", full.names = TRUE))
+  unlist(temp)
+  unlist(temp2)
+  return(your_SHP_file)
+}
