3cd9ab3563b8829e3e2908ab473700a6af177ffa
Reese Hirota <122996073+rhirotacouncil@users.noreply.github.com>
2023-09-13 14:51:27 -0400

    big cleanup

--- code/02_311_interactive.R
+++ code/02_311_interactive.R
@@ -2,6 +2,13 @@ library(tidyverse)
 library(htmltools)
 library(ggiraph)
 
+# run 01_311_inital_request.Rmd first
+
+od_dl[complaint_type %in% "Missed Collection (All Materials)", complaint_type := "Missed Collection"]
+od_dl[grepl("Litter", complaint_type, ignore.case = TRUE) | grepl("litter", descriptor, ignore.case = TRUE), complaint_type := "Litter"]
+od_dl[complaint_type %in% c("Overflowing Litter Baskets", "Dirty Condition", "Dirty"), complaint_type := "Dirty Conditions"]
+od_dl_sub <- od_dl[complaint_type %in% c("Dirty Conditions", "Missed Collection", "Litter")]
+
 # ---- 311 ----
 # 2018 to present
 # street cleanliness 
@@ -10,11 +17,6 @@ sum <- sum(od_dl_sub[od_dl_sub$complaint_type=="Dirty Conditions"]$n_comp) +
 complaint <- "Dirty Conditions"
 type <- "311"
 df_1 <- data.frame(sum, complaint, type)
-
-# sum <- sum(od_dl_sub[od_dl_sub$complaint_type=="Litter"]$n_comp)
-# complaint <- "Litter"
-# type <- "311"
-# df_1_2 <- data.frame(sum, complaint, type)
 
 # illegal dumping
 sum <- sum(id_over_time_df$n_comp)
@@ -28,16 +30,6 @@ complaint <- "Derelict Vehicles"
 type <- "311"
 df_3 <- data.frame(sum, complaint, type)
 
-# # catch basin complaints
-# sum <- sum(cb_over_time_df$n_comp)
-# complaint <- "Catch Basin"
-# df_4 <- data.frame(sum, complaint)
-
-# # grease
-# sum <- nrow(dep_311_grease)
-# complaint <- "Grease"
-# df_5 <- data.frame(sum, complaint)
-
 sum <- c(3040, 208665, 927)
 complaint <- c("Derelict Vehicles", "Dirty Conditions", "Illegal Dumping")
 type <- "OATH"
@@ -50,7 +42,7 @@ complaints <- df_list %>% reduce(full_join, by=c('sum', "complaint", "type"))
 options(scipen=10000)
 plot <- complaints %>% 
   ggplot(aes(x = reorder(complaint,sum), 
-             y=sum, fill = type)) + # geom_bar(stat = "identity", position = 'dodge')
+             y=sum, fill = type)) + 
   geom_bar_interactive(stat = "identity", position = 'dodge',
     width = 0.6,
     tooltip =
