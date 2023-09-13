2fdcdb211fad1a67ace71ef4f77c5789fedc809d
Reese Hirota <122996073+rhirotacouncil@users.noreply.github.com>
2023-09-13 16:05:45 -0400

    additional interactive

--- code/02_311_interactive.R
+++ code/02_311_interactive.R
@@ -1,14 +1,17 @@
 library(tidyverse)
 library(htmltools)
 library(ggiraph)
+library(zoo)
 
 # run 01_311_inital_request.Rmd first
 
-# street cleanliness 
+# ---- street cleanliness 
 od_dl <- dsny_311
 od_dl[complaint_type %in% c("Overflowing Litter Baskets", "Dirty Condition", "Dirty"), complaint_type := "Dirty Conditions"]
 od_dl[grepl("Litter", complaint_type, ignore.case = TRUE) | grepl("litter", descriptor, ignore.case = TRUE), complaint_type := "Litter"]
 od_dl_sub <- od_dl[complaint_type %in% c("Dirty Conditions", "Litter")]
+# > sum((od_dl_sub[od_dl_sub$date_ym > "2022-08-01" & od_dl_sub$complaint_type == "Dirty Conditions",])$n_comp)
+# [1] 47537 - from old version, littering adds ~10k
 
 od_dl_sub[, n_comp := .N, by = c("complaint_type", "date_ym")] # count by complaint type, date
 od_dl_sub <- od_dl_sub[, .(complaint_type, date_ym, n_comp)] # subset to complaint type, date, n
@@ -20,14 +23,15 @@ complaint <- "Dirty Conditions"
 type <- "311"
 df_1 <- data.frame(sum, complaint, type)
 
-# illegal dumping
+
+# ---- illegal dumping
 id_over_time_df <- id_over_time_df[id_over_time_df$date_ym >= "2022-08-01" & id_over_time_df$date_ym < "2023-08-01", ]
 sum <- sum(id_over_time_df$n_comp)
 complaint <- "Illegal Dumping"
 type <- "311"
 df_2 <- data.frame(sum, complaint, type)
 
-# derelict vehicles
+# ---- derelict vehicles
 dv_over_time_df <- dv_over_time_df[dv_over_time_df$date_ym >= "2022-08-01" & dv_over_time_df$date_ym < "2023-08-01", ]
 sum <- sum(dv_over_time_df$n_comp)
 complaint <- "Derelict Vehicles"
@@ -39,10 +43,11 @@ complaint <- c("Derelict Vehicles", "Dirty Conditions", "Illegal Dumping")
 type <- "OATH"
 df_4 <- data.frame(sum, complaint, type)
 
-# merge complaints
+# ---- merge complaints
 df_list <- list(df_1, df_2, df_3, df_4)    
 complaints <- df_list %>% reduce(full_join, by=c('sum', "complaint", "type"))
 
+# ---- make interactive plot
 options(scipen=10000)
 plot <- complaints %>% 
   ggplot(aes(x = reorder(complaint,sum), 
@@ -74,5 +79,96 @@ plot_interactive <- girafe(ggobj = plot,
                            options = list(
                              opts_tooltip(css = tooltip_css)
                            ))
-htmltools::save_html(plot_interactive, "../visuals/complaints_311_citywide_bar.html")
+# htmltools::save_html(plot_interactive, "../visuals/complaints_311_citywide_bar.html")
+
+# ---- plot additional concerns
+# illegal dumping
+id_plot <- id_over_time_df %>% 
+  ggplot(aes(x = as.Date(date_ym), 
+              y= n_comp)) +
+  geom_line_interactive(size = 1.2,
+                        alpha = 0.4)+
+  geom_point_interactive(
+    aes(tooltip = paste(date_ym,
+      "\nComplaints:",n_comp
+    )),
+    fill = "white",
+    size = 2.5,
+    stroke = 1.5,
+    shape = 21) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %y") +
+  labs(x="Date",  y="Number of Complaints") +
+  theme_nycc()
+
+tooltip_css <- "background-color:#CACACA;"
+
+id_interactive <- girafe(ggobj = id_plot,   
+                           width_svg = 8,
+                           height_svg = 5, 
+                           options = list(
+                             opts_tooltip(css = tooltip_css)
+                           ))
+# htmltools::save_html(id_interactive, "../visuals/id_complaints_citywide_line.html")
+
+# yellow/brown grease
+grease_over_time_df <- dep_311_grease[dep_311_grease$date_ym >= "2022-08-01" & dep_311_grease$date_ym < "2023-08-01",] %>% 
+  group_by(date_ym) %>% 
+  summarise(n_comp = n())
+
+grease_plot <- grease_over_time_df %>% 
+  ggplot(aes(x = as.Date(date_ym), 
+             y= n_comp)) +
+  geom_line_interactive(size = 1.2,
+                        alpha = 0.4)+
+  geom_point_interactive(
+    aes(tooltip = paste(date_ym,
+                        "\nComplaints:",n_comp
+    )),
+    fill = "white",
+    size = 2.5,
+    stroke = 1.5,
+    shape = 21) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %y") +
+  labs(x="Date",  y="Number of Complaints") +
+  theme_nycc()
+
+grease_interactive <- girafe(ggobj = grease_plot,   
+                         width_svg = 8,
+                         height_svg = 5, 
+                         options = list(
+                           opts_tooltip(css = tooltip_css)
+                         ))
+
+# htmltools::save_html(grease_interactive, "../visuals/grease_complaints_citywide_line.html")
+
+# derelict and abandoned vehicles
+dv_plot <- dv_over_time_df %>% 
+  ggplot(aes(x = as.Date(date_ym), 
+             y= n_comp)) +
+  geom_line_interactive(size = 1.2,
+                        alpha = 0.4)+
+  geom_point_interactive(
+    aes(tooltip = paste(date_ym,
+                        "\nComplaints:",n_comp
+    )),
+    fill = "white",
+    size = 2.5,
+    stroke = 1.5,
+    shape = 21) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %y") +
+  labs(x="Date",  y="Number of Complaints") +
+  theme_nycc()
+dv_interactive <- girafe(ggobj = dv_plot,   
+                         width_svg = 8,
+                         height_svg = 5, 
+                         options = list(
+                           opts_tooltip(css = tooltip_css)
+                         ))
+
+# htmltools::save_html(dv_interactive, "../visuals/dv_complaints_citywide_line.html")
+
+
+
+
+
 
